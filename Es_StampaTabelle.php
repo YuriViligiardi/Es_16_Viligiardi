edad3d61edd1bc9e087be19eb8f2616215eb9505
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-04 08:53:41 +0200

    Aggiunta funzione stampatabella, con la relativa tabella

--- Es_StampaTabelle.php
+++ Es_StampaTabelle.php
@@ -7,6 +7,23 @@
 </head>
 <body>
     <?php
+        $righe = rand(1,10);
+        echo "<h3>Righe: $righe</h3>";
+        $colonne = rand(1,10);
+        echo "<h3>Colonne: $colonne</h3>";
+        stampaTabella($righe, $colonne);
+
+        function stampaTabella($r,$c){
+            echo "<table style='border: 1px solid black; border-collapse: collapse'>";
+            for ($i=0; $i < $r; $i++) { 
+                echo "<tr style:'border: 1px solid black;'>";
+                for ($x=0; $x < $c; $x++) { 
+                    echo "<td style='border: 1px solid black; padding: 50px;'>($i,$x)</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
     ?>
 </body>
 </html>
